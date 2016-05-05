eb98328c68f83472e957573d890fc56403ece818
Steve Ruff <sruff@waveguide.com>
2016-05-05 14:49:58 -0400

    Submitting "refresher" homework

---
+++ Summer/Summer/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Summer
+{
+    public partial class Form1 : Form
+    {
+        int result = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            result += int.Parse(textBox1.Text);
+            textBox2.Text = String.Concat(result);
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+            result = 0;
+        }
+    }
+}
